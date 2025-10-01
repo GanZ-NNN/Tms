8b2a7ae90acef3ae9bae920ff660f3e218fc8239
GanZ-NNN <ganz@DESKTOP-R42E3FK>
2025-10-02 01:01:30 +0700

    feat:fix navbar

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -84,7 +84,7 @@
                     </div>
 
                     {{-- Update Password & Delete Account (ย้ายมาไว้ล่างสุด) --}}
-                    <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    {{-- <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                         <div class="max-w-xl">
                             @include('profile.partials.update-password-form')
                         </div>
@@ -94,7 +94,7 @@
                         <div class="max-w-xl">
                             @include('profile.partials.delete-user-form')
                         </div>
-                    </div>
+                    </div> --}}
                 </div>
 
             </div>
