0dbca1965330d6f523b7567c1fcb7bf2d24b9878
GanZ-NNN <ganz@DESKTOP-R42E3FK>
2025-10-17 15:40:40 +0700

    feat: fix for deploy

---
+++ app/Console/Commands/CreateAdminUser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CreateAdminUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-admin-user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->ask('Enter admin name:');
+        $email = $this->ask('Enter admin email:');
+        $password = $this->secret('Enter admin password:');
+
+        \App\Models\User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => \Illuminate\Support\Facades\Hash::make($password),
+            'role' => 'admin',
+        ]);
+
+        $this->info('Admin user created successfully!');
+    }
+}
